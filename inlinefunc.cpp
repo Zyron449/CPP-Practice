a5173b4df2e6b8663dfc749ab7e2177332fcf428
Zyron449 <dahrunabisheik01@gmail.com>
2026-06-30 18:30:53 +0530

    Inline function Added

---
+++ inlinefunc.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+inline int square(int x) { 
+return x * x; 
+} 
+int main() { 
+int n; 
+cout << "Enter a number: "; 
+cin >> n; 
+cout << "Square = " << square(n); 
+} 
